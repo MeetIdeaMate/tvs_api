42dfbb001916d7314e868c4231afe220babf77a7
techlambdas <techlambdasbussiness@gmail.com>
2024-10-10 10:50:21 +0530

    statement summary

--- src/main/java/com/techlambdas/delearmanagementapp/service/StatementSummaryServiceImpl.java
+++ src/main/java/com/techlambdas/delearmanagementapp/service/StatementSummaryServiceImpl.java
@@ -7,11 +7,8 @@ import com.techlambdas.delearmanagementapp.model.submodel.StatementDetails;
 import com.techlambdas.delearmanagementapp.repository.BankStatementSummaryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -36,32 +33,26 @@ public class StatementSummaryServiceImpl implements StatementSummaryService{
     @Override
     public void createStatementSummary(String statementId) {
         BankStatementSummary bankStatementSummary = new BankStatementSummary();
-
         Statement statement = statementService.getByStatementId(statementId);
-
-
         Map<String, List<BankTransaction>> groupedTransactions = statement.getTransactions().stream()
                 .collect(Collectors.groupingBy(transaction -> {
                     AccountType accountType = transaction.getCredit() > 0 ? AccountType.CREDIT : AccountType.DEBIT;
-                    return accountType.name() + "-" + transaction.getPaymentType(); // Concatenate as key
+                    return accountType.name() + "-" + transaction.getPaymentType();
                 }));
-
         List<BankStatementSummaryDetails> bankStatementSummaryList = new ArrayList<>();
-
+        Map<String,Account> missMatchAccountMap = new HashMap<>();
         for (Map.Entry<String, List<BankTransaction>> entry : groupedTransactions.entrySet()) {
             String key = entry.getKey();
             List<BankTransaction> transactionList = entry.getValue();
-
             String[] keyParts = key.split("-");
             AccountType accountType = AccountType.valueOf(keyParts[0]);
             String paymentType = keyParts[1];
-
             BankStatementSummaryDetails bankStatementSummaryDetails = new BankStatementSummaryDetails();
             double cumulativeAmount = 0.0;
             double cumulativeApplicationAmt = 0.0;
             List<StatementDetails> summaryDetailsList = new ArrayList<>();
-            StatementDetails missMatchstatementDetails = new StatementDetails();
-
+            List<StatementDetails> missMatchAccountDetails = new ArrayList<>();
+            StatementDetails missMatchAccount = new StatementDetails();
             for (BankTransaction transaction : transactionList) {
                 double transactionAmount = transaction.getCredit() > 0 ? transaction.getCredit() : transaction.getDebit();
                 cumulativeAmount += transactionAmount;
@@ -78,14 +69,11 @@ public class StatementSummaryServiceImpl implements StatementSummaryService{
                     summaryDetailsRes.setAccountHead(matchingAccountHead.getAccountHeadName());
                     String accountHeadCode = matchingAccountHead.getAccountHeadCode();
                     List<Account> accounts = accountService.findByTransactDateBetween(statement.getFromDate(), statement.getToDate());
-                    System.out.println("accountsList" + accounts);
-                    List<Account> matchingAccountOpt = accounts.stream()
+                    List<Account> matchingAccountHeadRecord = accounts.stream()
                             .filter(acc -> acc.getAccountHeadCode().equals(accountHeadCode))
                             .collect(Collectors.toList());
-                    System.out.println("MatchedAccount" + matchingAccountOpt);
-                    if (!matchingAccountOpt.isEmpty()) {
-
-                        for (Account MatchAcc : matchingAccountOpt) {
+                    if (!matchingAccountHeadRecord.isEmpty()) {
+                        for (Account MatchAcc : matchingAccountHeadRecord) {
                             if (transaction.getDate().equals(MatchAcc.getTransactDate())
                                     && transaction.getPaymentType().equals(MatchAcc.getAccountHeadName())
                                     && transaction.getDescription().contains(MatchAcc.getTransactRefNo())) {
@@ -95,35 +83,31 @@ public class StatementSummaryServiceImpl implements StatementSummaryService{
                                 summaryDetailsRes.setSalesBillNo(MatchAcc.getTransactRefNo());
                                 summaryDetailsRes.setMissMatch((transaction.getCredit() > 0 ? transaction.getCredit() : transaction.getDebit()) != MatchAcc.getAmount());
                             } else {
-                                missMatchstatementDetails.setDate(null);
-                                missMatchstatementDetails.setAmount(0);
-                                missMatchstatementDetails.setCheque(null);
-                                missMatchstatementDetails.setAccountHead(matchingAccountHead.getAccountHeadName());
-                                missMatchstatementDetails.setDescription(MatchAcc.getTransactDesc());
-                                missMatchstatementDetails.setApplicationAmt(MatchAcc.getAmount());
-                                missMatchstatementDetails.setPartyName(MatchAcc.getTransactorId());
-                                missMatchstatementDetails.setSalesBillNo(MatchAcc.getTransactRefNo());
-                                missMatchstatementDetails.setMissMatch((transaction.getCredit() > 0 ? transaction.getCredit() : transaction.getDebit()) != MatchAcc.getAmount());
-
+                                summaryDetailsRes.setMissMatch(true);
+                                if (!missMatchAccountMap.containsKey(MatchAcc.getTransacId())){
+                                    missMatchAccountMap.put(MatchAcc.getTransacId(), MatchAcc);
+                                }
                             }
-
-
-                        } }else{
-
-
-                            summaryDetailsRes.setApplicationAmt(0.0);
-
+                            }
                         }
-
-                    }
-
-                    summaryDetailsList.add(summaryDetailsRes);
-
-
                 }
-            summaryDetailsList.add(missMatchstatementDetails);
-
-
+                summaryDetailsList.add(summaryDetailsRes);
+                }
+            for (Map.Entry<String, Account> accountEntry : missMatchAccountMap.entrySet()) {
+                System.out.println("account entry" +accountEntry);
+                Account account = accountEntry.getValue();
+                missMatchAccount.setDate(null);
+                missMatchAccount.setAmount(0);
+                missMatchAccount.setCheque(null);
+                missMatchAccount.setAccountHead(account.getAccountHeadName());
+                missMatchAccount.setDescription(account.getTransactDesc());
+                missMatchAccount.setApplicationAmt(account.getAmount());
+                missMatchAccount.setPartyName(account.getTransactorId());
+                missMatchAccount.setSalesBillNo(account.getTransactRefNo());
+                missMatchAccount.setMissMatch(true);
+                missMatchAccountDetails.add(missMatchAccount);
+            }
+            summaryDetailsList.addAll(missMatchAccountDetails);
                 bankStatementSummaryDetails.setDescription("Summary for " + paymentType);
                 bankStatementSummaryDetails.setAmount(cumulativeAmount);
                 bankStatementSummaryDetails.setAccountHeadName(paymentType);
@@ -131,17 +115,12 @@ public class StatementSummaryServiceImpl implements StatementSummaryService{
                 bankStatementSummaryDetails.setSummaryDetails(summaryDetailsList);
                 bankStatementSummaryDetails.setAccountType(accountType);
                 bankStatementSummaryDetails.setMissMatch(cumulativeAmount != cumulativeApplicationAmt);
-
                 bankStatementSummaryList.add(bankStatementSummaryDetails);
             }
-
             bankStatementSummary.setBankStatementSummaryList(bankStatementSummaryList);
             bankStatementSummary.setStatementId(statementId);
-
             bankStatementSummaryRepository.save(bankStatementSummary);
         }
-
-
     @Override
     public BankStatementSummary getStatementSummary(String statementId) {
         return bankStatementSummaryRepository.findByStatementId(statementId);
